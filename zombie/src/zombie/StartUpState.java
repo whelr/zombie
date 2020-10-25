66f0f7b479db9cb3e15d5f163a70984356a4ce9d
Michael Wheeler <michael.j.wheeler@wsu.edu>
2020-10-25 00:27:56 -0700

    Cool startup screen with animation.

--- zombie/src/zombie/StartUpState.java
+++ zombie/src/zombie/StartUpState.java
@@ -9,15 +9,43 @@ import org.newdawn.slick.Input;
 
 public class StartUpState extends BasicGameState {
 
+	Humanoid survivor;
+	Humanoid zombie;
+	Item food;
+	Item water;
+	Item rope;
+	
+	boolean starting = false;
+	
 	@Override
 	public void init(GameContainer container, StateBasedGame game) throws SlickException {
-		// TODO Auto-generated method stub
-		
+		zombie = new Humanoid(50, 150, 1); //top left	
+		survivor = new Humanoid(150, 150, 0); //bottom right
+		water = new Item(200, 150, 0); //7/1
+		food = new Item(250, 150, 1); //6/11
+		rope = new Item(500, 150, 2); //6/11
 	}
 	
 	@Override
 	public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
-		g.drawString("Speak 'Space' to Enter", 205, 300);
+		g.drawString("Welcome to Dijkstra's Zombie Experiment!", 25, 200);
+		g.drawString("You are an experimental purple block humanoid,", 25, 220);
+		g.drawString("Your goal is to not be consumed by the green zombie blockman.", 25, 240);
+		g.drawString("Proceed through the level collecting food and water.", 25, 260);
+		g.drawString("Once aquired, a door will take you to the next level.", 25, 280);
+		g.drawString("5 Levels Await...", 25, 300);
+		g.drawString("Speak 'Space' to Enter", 190, 350);
+		
+		g.drawString("Controls:", 25, 400);
+		g.drawString("WASD - Movement.", 35, 420);
+		g.drawString("1, 2, 3, 4, 5 to level skip.", 35, 440);
+		g.drawString("Q to enabled Dijkstra's overlay.", 35, 460);
+
+		survivor.render(g);
+		zombie.render(g);
+		food.render(g);
+		water.render(g);
+		rope.render(g);
 	}
 
 	@Override
@@ -25,10 +53,26 @@ public class StartUpState extends BasicGameState {
 		Input input = container.getInput();
 		ZombieGame zg = (ZombieGame)game;
 		
+		if(starting) {
+			survivor.setX(survivor.getX() + delta * 0.07f);	
+			zombie.setX(zombie.getX() + delta * 0.07f);	
+		}
+		
 		if(input.isKeyDown(Input.KEY_SPACE))
+			starting = true;
+		
+		if(survivor.collides(water) != null)
+			water.acquire(Item.WATER);
+		
+		if(survivor.collides(food) != null)
+			food.acquire(Item.FOOD);
+		
+		if(survivor.collides(rope) != null)
 			zg.enterState(ZombieGame.PLAYINGSTATE);
+		
 	}
 
+	
 	@Override
 	public int getID() {
 		return ZombieGame.STARTUPSTATE;
